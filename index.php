b70c6efe7cfb98c10cf11864f990a2c79d43825d
Khosrov Khosrovyan <Kkhosrovyan@elpuig.xeill.net>
2020-11-05 20:37:05 +0100

    .

--- index.php
+++ index.php
@@ -7,63 +7,46 @@
 <h1></h1>  <!----Por si quieres poner un titulo-->
 
 <form action="/index.php">
-    <input id="Afegir" name="Afegir">
-    <select id="khosrov" name="khosrov" size="1">
+    <input name="afegir">
+    <select name="prioritat" size="1">
         <option value="Alta">Alta</option>
         <option value="Baixa">Baixa</option>
     </select>
-    <input type="submit" value="Afegir">    
+    <input class="bguay" type="submit" value="Afegir">    
 </form>
 
 <p></p><!---espacio-->
 
-<ul>
-    <li class="baixa">
-    Comprar el PA
-    <a href="?eliminar=Comprar el pa"><img src="basura.jpg" alt="delete" style="widows: 22px;height:22px"></a>        
-    </li>
-
-    <li class="alta">
-    Aprendre HTML
-    <a href="?eliminar=Aprendre HTML"><img src="basura.jpg" alt="delete" style="widows: 22px;height:22px"></a>
-    </li>
-    
-    <li class="alta">
-    Fer exercici
-    <a href="?eliminar=Fer exercicis"><img src="basura.jpg" alt="delete" style="widows: 22px;height:22px"></a>
-    </li>
-
-    <li class="baixa">
-    Aprendre CSS
-    <a href="?eliminar=Aprendre CSS"><img src="basura.jpg" alt="delete" style="widows: 22px;height:22px"></a>
-    </li>
-
-    <li class="alta">
-    Aprovar SMX
-    <a href="?eliminar=Aprovar SMX"><img src="basura.jpg" alt="delete" style="widows: 22px;height:22px"></a>
-    </li>
-</ul>
-
 <?php
 
 
-$db = new mysqli("localhost", "root", "1234", "BASEDEDATOS");
+$db = new mysqli("localhost", "admin",  "admin", "BASEDEDATOS");
 
-$stmt = $db->prepare("INSERT INTO TAULA1 VALUES(descripcio,prioritat)");
-$stmt->bind_param("ss", ALTA, BAIXA);
-$stmt->execute();
 
-$stmt = $db->prepare("INSERT INTO TAULA1 VALUES(descripcio,prioritat)");
-$stmt->bind_param("ss", $_GET['TEXT'], $_GET['Afegir']);
-$stmt->execute();
 
-$usuario = "root";
-$contrasena = "1234";  
-$servidor = "localhost";
-$basededatos = "BASEDEDATOS";
+if (!empty($_GET['afegir'])) {
+    $stmt = $db->prepare("INSERT INTO TAULA1 VALUES(?,?)");
+    $stmt->bind_param("ss", $_GET['afegir'], $_GET['prioritat']);
+    $stmt->execute();
+}
 
-$conexion = mysqli_connect( $localhost, $root, "1234" );
+if (!empty($_GET['eliminar'])) {
+    $stmt = $db->prepare("DELETE FROM TAULA1 WHERE descripcio = ?");
+    $stmt->bind_param("s", $_GET['eliminar']);
+    $stmt->execute();
+}
 
-$db = mysqli_select_db( $conexion, $BASEDEDATOS );
+foreach ($db->query("SELECT * FROM TAULA1 WHERE prioritat='Alta'") as $fila) {
+        echo "<li class='${fila['prioritat']}'>";
+        echo "${fila['descripcio']}";
+        echo "<a href='?eliminar=${fila['descripcio']}'><img src='basura.jpg' alt='delete' style='widows: 22px;height:22px'></a>
+    </li>";
+}
+
+foreach ($db->query("SELECT * FROM TAULA1 WHERE prioritat='Baixa'") as $fila) {
+        echo "<li class='${fila['prioritat']}'>";
+        echo "${fila['descripcio']}";
+        echo "<a href='?eliminar=${fila['descripcio']}'><img src='basura.jpg' alt='delete' style='widows: 22px;height:22px'></a>
+    </li>";
+}
 ?>
-
--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
-apt install php mysql 
+apt install apache2 -y
+apt install mysql-server -y
+apt install php-mysql -y
+apt install php
+
+
 mysql -e "CREATE DATABASE BASEDEDATOS"
 mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'"
 mysql -e "GRANT ALL PRIVILEGES ON BASEDEDATOS.* TO 'admin'@'localhost; FLUSH PRIVILEGES"
